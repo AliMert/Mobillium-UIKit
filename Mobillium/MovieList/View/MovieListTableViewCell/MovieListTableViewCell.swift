65f1f78c3e878a1071a5511e10449133659c8c95
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-04-26 03:22:10 +0300

    adjust dynamic height on tableView

--- Mobillium/MovieList/View/MovieListTableViewCell/MovieListTableViewCell.swift
+++ Mobillium/MovieList/View/MovieListTableViewCell/MovieListTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MovieListTableViewCell: UITableViewCell {
+final class MovieListTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
--- Mobillium/MovieList/View/SliderView/SliderCollectionViewCell/SliderCollectionViewCell.swift
+++ Mobillium/MovieList/View/SliderView/SliderCollectionViewCell/SliderCollectionViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SliderCollectionViewCell: UICollectionViewCell {
+final class SliderCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
